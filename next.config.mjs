e3e9f10919a8165eace077774ce077b42b01b68b
Qasim-Jakhar <mqasimbilal05@gmail.com>
2026-08-17 16:50:05 +0500

    Disable allowedDevOrigins configuration
    Comment out allowedDevOrigins in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  allowedDevOrigins: [
-    "per-functionality-competitions-optical.trycloudflare.com",
-  ],
+  // allowedDevOrigins: [
+  //   "per-functionality-competitions-optical.trycloudflare.com",
+  // ],
 };
 
 export default nextConfig;
